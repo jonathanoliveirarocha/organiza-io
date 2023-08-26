099c4f53305ae25a8e87966f45414bbf160cecaf
Jonathan de Oliveira <jonathandeoliveirarocha2002@gmail.com>
2023-08-26 20:09:38 -0300

    buttons animations

---
+++ src/scripts/main.js
@@ -0,0 +1,10 @@
+function add(){
+    document.querySelector(".main").style.filter = "blur(5px)"
+    document.querySelector(".efect").style.display ="flex"
+    
+}
+
+function closeWindow(){
+    document.querySelector(".efect").style.display ="none"
+    document.querySelector(".main").style.filter = "blur(0px)"
+}
