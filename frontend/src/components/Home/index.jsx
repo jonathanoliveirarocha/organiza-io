eb0a5a632753a1f07a8c510b1642a50f28cbbf1f
Jonathan de Oliveira <jonathandeoliveirarocha2002@gmail.com>
2023-12-16 18:04:26 -0300

    Starting Home Styiling

--- frontend/src/components/Home/index.jsx
+++ frontend/src/components/Home/index.jsx
@@ -5,7 +5,48 @@ const Home = () => {
   return (
     <>
       <Header />
+      <div className="w-full min-h-screen flex items-center justify-center py-[80px]">
+        <div className="space-y-8">
+          <div className="shadow-sm">
+            <Timeline />
+          </div>
+          <div className="w-fit mx-auto">
+            <button className="bg-green-700/80 text-white text-sm py-1 px-3 rounded-md hover:opacity-90 shadow-lg">
+              Adicionar
+            </button>
+          </div>
+        </div>
+      </div>
       <Footer />
+    </>
+  );
+};
+
+const Timeline = () => {
+  return (
+    <>
+      <div className="xl:flex">
+        <DayWeek day="Segunda-Feira" />
+        <DayWeek day="Terça-Feira" />
+        <DayWeek day="Quarta-Feira" />
+        <DayWeek day="Quinta-Feira" />
+        <DayWeek day="Sexta-Feira" />
+        <DayWeek day="Sábado" />
+        <DayWeek day="Domingo" />
+      </div>
+    </>
+  );
+};
+
+const DayWeek = ({ day }) => {
+  return (
+    <>
+      <div className="xl:w-[170px] w-64 border border-gray-200">
+        <div className="bg-gray-200 py-2">
+          <h2 className="text-center text-xl font-bold">{day}</h2>
+        </div>
+        <div className="xl:h-[500px] h-48"></div>
+      </div>
     </>
   );
 };
