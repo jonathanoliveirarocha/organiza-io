d014e5e8549992d6cf42d47252454b6c80bba6df
Jonathan de Oliveira <jonathandeoliveirarocha2002@gmail.com>
2023-12-16 16:50:31 -0300

    Ending Login page

--- frontend/src/components/Login/index.jsx
+++ frontend/src/components/Login/index.jsx
@@ -1,7 +1,52 @@
+import Banner from "../Partials/Banner";
+
 const Login = () => {
   return (
     <>
-      <p>Login</p>
+      <div className="w-full h-screen flex">
+        <Banner />
+        <Form />
+      </div>
+    </>
+  );
+};
+
+const Form = () => {
+  return (
+    <>
+      <div className="xl:w-2/5 w-full h-full flex justify-center items-center p-6">
+        <form className="w-[400px] text-[#87878a] space-y-8">
+          <h1 className="text-[#d75413] text-3xl font-bold text-center">
+            Login
+          </h1>
+          <div className="w-full space-y-2">
+            <label htmlFor="email">E-mail:</label>
+            <input
+              className="w-full h-[30px] shadow-lg text-sm rounded-md p-3"
+              type="email"
+              name="email"
+              placeholder="Digite seu E-mail"
+            />
+          </div>
+          <div className="w-full space-y-2">
+            <label htmlFor="password">Senha:</label>
+            <input
+              className="w-full h-[30px] shadow-lg text-sm rounded-md p-3"
+              type="password"
+              name="password"
+              placeholder="Digite sua Senha"
+            />
+          </div>
+          <button className="w-full h-[30px] bg-[#d75413] rounded-md text-[#fffafa] text-sm hover:opacity-90 shadow-lg">
+            Entrar
+          </button>
+          <p className="text-center">
+            <a href="#" className="text-[#d75413] hover:opacity-80">
+              Cadastre-se
+            </a>
+          </p>
+        </form>
+      </div>
     </>
   );
 };
---
+++ frontend/src/components/Partials/Banner/index.jsx
@@ -0,0 +1,20 @@
+import BannerImg from "../../../assets/banner-img.jpg";
+
+const Banner = () => {
+  return (
+    <>
+      <div className="hidden relative w-3/5 h-full xl:inline-block">
+        <div className="absolute w-full top-[45%] left-[10%] mt-[-25px] font-bold text-white">
+          <h1 className="w-[53%] text-6xl">Organize suas ideias</h1>
+          <h2 className="text-[#d75413] w-[40%] text-[45px] leading-[60px]">
+            Tudo em um só lugar
+          </h2>
+        </div>
+        <img src={BannerImg} className="h-full w-full" alt="Banner da Página" />
+        {/* Used Image: https://www.pexels.com/pt-br/foto/livro-de-cor-cinza-padrao-perto-de-borracha-verde-159045/ */}
+      </div>
+    </>
+  );
+};
+
+export default Banner;
